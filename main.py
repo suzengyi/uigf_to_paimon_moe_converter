2bfa7e6a6b9afcf39abc3a1f05b9f811651504c7
suzengyi <suzengyi@sheryl.lo>
2023-01-04 21:07:05 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import json
+import urllib
+_BANNER_URL="https://github.com/MadeBaruna/paimon-moe/raw/fe3298c16f840fc6c3e47c96ff83718c8a801279/src/data/banners.js"
+_LOCALE_URL="https://github.com/MadeBaruna/paimon-moe/raw/main/src/locales/items/zh.json"
+
+def main():
+    print("hello")
+if __name__=="__main__":
+    main()
