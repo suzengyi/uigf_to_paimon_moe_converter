c204d2b0cbd271d74b15eb3b288451026006bb2d
suzengyi <suzengyi@sheryl.lo>
2023-01-06 18:01:57 +0800

    添加日志显示

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import json5
 import urllib.request
 import openpyxl
+import logging
 from datetime import datetime
+
+#日志配置
+_log = logging.getLogger(__name__)
+_log.setLevel(logging.INFO)
+_ch = logging.StreamHandler()
+_ch.setLevel(logging.DEBUG)
+_LOG_FORMAT = '%(asctime)s - %(levelname)s - %(funcName)s:%(lineno)d - %(message)s'
+_ch.setFormatter(logging.Formatter(_LOG_FORMAT))
+_log.addHandler(_ch)
 
 _BANNER_URL="https://github.com/MadeBaruna/paimon-moe/raw/main/src/data/banners.js"
 _LOCALE_URL="https://github.com/MadeBaruna/paimon-moe/raw/main/src/locales/items/zh.json"
@@ -19,13 +29,16 @@ _PULL_TYPE_DICT={
     "Character":"角色"
 }
 
-def _get_key (dict, value):
-    for k, v in dict.items():
+def _get_key (dic, value):
+    for k, v in dic.items():
          if v == value:
             return k
+    _log.warning(f"未找到{value}")
+    _log.debug(f"查找{value}堆栈如下：",stack_info=True)
     return None
   
 def _get_url(url:str)->str:
+    _log.debug(f"获取{url}")
     url=urllib.request.urlopen(url)
     return url.read().decode(_ENCODE)
 
@@ -45,6 +58,7 @@ def get_locale()->dict:
     return json5.loads(locale_raw)
 
 def get_uigf_from_file(path:str)->dict:
+    _log.debug(f"从{path}中获取UIGF")
     with open(path,"r",encoding=_ENCODE) as f:
         uigf_dict=json5.load(f,encoding=_ENCODE)
     return uigf_dict
@@ -53,32 +67,18 @@ def _init_workbook()->openpyxl.workbook.Workbook:
     ret=openpyxl.workbook.Workbook()
     ws = ret.active
     titles = [x[0] for x in _GACHA_TYPE_DICT.values()]
+    _log.debug(f"工作表标题为：{titles}")
     ws.title = titles[0]
+    _log.debug(f"修改第一个工作表为：{titles[0]}")
     ws.append(_PAIMON_TITLE)
     for i in range(1,len(titles)):
         if titles[i] in ret.sheetnames:
             continue
+        _log.debug(f"创建工作表：{titles[i]}")
         ret.create_sheet(titles[i])
         ws=ret[titles[i]]
         ws.append(_PAIMON_TITLE)
     return ret
-
-def get_pity(rank_type:str,
-             ws:openpyxl.worksheet.worksheet.Worksheet)->int:
-    pity_col = _PAIMON_TITLE.index("Pity")
-    row_idx=ws.max_row
-    if rank_type=="3":
-        return 1
-    elif row_idx < 2:
-        return 1
-    else:
-        ret = 1
-        while row_idx >1 and int(ws[row_idx][pity_col-1].value) <4:
-            ret += 1
-            row_idx -=1
-        if ret >10:
-            print(f"{ws.title}数据错误，Pity值大于10")
-        return ret
 
 def get_banner_by_date(
         banners_dict:dict,  #banner字典
@@ -88,16 +88,18 @@ def get_banner_by_date(
     flag = False
     if gacha_type == '302':
         #武器活动祈愿
+        _log.debug(f"查找武器活动祈愿：{time}")
         idx = get_banner_by_date.weapon_idx
         banners = banners_dict['weapons']
     else:
         #角色活动祈愿
+        _log.debug(f"查找角色活动祈愿（{gacha_type}）：{time}")
         idx = get_banner_by_date.character_idx
         banners = banners_dict['characters']
     while not banners[idx]['start'] < time < banners[idx]['end']:
         flag=True
         if time < banners[idx]['start']:
-            print("向前移动")
+            _log.warning(f"祈愿{gacha_type}中存在时间{time}的祈愿顺序错误")
             idx-=1
         else :
             idx+=1
@@ -225,7 +227,7 @@ def add_extra_data(wb:openpyxl.workbook.Workbook,banners_dict:dict):
         ))
     
     #添加Information Sheet
-    info_sheet_name = "Information Sheet"
+    info_sheet_name = "Information"
     wb.create_sheet(info_sheet_name)
     ws=wb[info_sheet_name]
     ws['A1'] = "Paimon.moe Wish History Export"
@@ -236,10 +238,20 @@ def add_extra_data(wb:openpyxl.workbook.Workbook,banners_dict:dict):
     ws.merge_cells('A1:B1')
 
 def main():
+    _log.info("开始读取祈愿信息")
     banners_dict=get_banners()
+    _log.info("祈愿信息读取完毕")
+    _log.debug(f"祈愿信息字典：\n{banners_dict}")
+    _log.info("开始读取本地化信息")
     locale_dict = get_locale()
+    _log.info("本地化信息读取完毕")
+    _log.debug(f"本地化信息字典：\n{banners_dict}")
+    _log.info("开始读取UIGF信息")
     uigf_dict=get_uigf_from_file("uigf.json")
+    _log.info("UIGF信息读取完毕")
+    _log.debug(f"UIGF信息字典：\n{uigf_dict}")
     workbook=_init_workbook()
+    _log.info(f"开始将UIGF信息转换为Excel")
     for gacha in uigf_dict['list']:
         worksheet=workbook[_GACHA_TYPE_DICT[gacha['gacha_type']][0]]
         row=[]
@@ -280,10 +292,15 @@ def main():
         if diff_flag == True:
             row.append("1")
         worksheet.append(row)
+    _log.info(f"信息转换完毕，开始填充Pity、Row、Group列")
     fix_pity_row_group(workbook)
+    _log.info(f"填充完毕，开始清理Excel文件")
     trim_workbook(workbook)
+    _log.info(f"清理Excel文件完毕，开始添加额外信息")
     add_extra_data(workbook, banners_dict)
+    _log.info(f"添加额外信息完毕，保存文件")
     workbook.save("test_output.xlsx")
+    _log.info(f"保存完毕，执行结束")
 
 if __name__=="__main__":
     main()
